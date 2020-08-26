1d1f1167015fbbd728112dfde19219853ce0694f
Paul Marganian <pmargani@nrao.edu>
2020-08-26 18:31:17 -0400

    added comments

--- Robot.py
+++ Robot.py
@@ -19,36 +19,47 @@ class Robot:
         self.gyroGain = 0.7
 
     def runTopMotors(self, speed, rotation_angle):
+        "Turns on top motors in opposite directions for as many degrees as you tell it"
         self.leftTopMotor.run_angle(-speed,rotation_angle,Stop.COAST,False)
         self.rightTopMotor.run_angle(speed,rotation_angle,Stop.COAST,True)
 
     def driveForward(self, speed, rotation_angle):
+        "Turns on bottom motors in same direction to dirve foward for degrees you tell it"
+        # don't wait here, so that both motors can turn at the same time
         self.leftMotor.run_angle(speed,rotation_angle,Stop.COAST,False)
+        # but wait here!
         self.rightMotor.run_angle(speed,rotation_angle,Stop.COAST,True)
     
     def spinRight(self, speed, rotation_angle):
+        "Just like drive forward, except right motor goes backward"
         self.leftMotor.run_angle(speed,rotation_angle,Stop.COAST,False)
         self.rightMotor.run_angle(-speed,rotation_angle,Stop.COAST,True)
         
     def spinLeft(self, speed, rotation_angle):
+        "Just like drive forward, except left motor goes backward"
         self.leftMotor.run_angle(-speed,rotation_angle,Stop.COAST,False)
         self.rightMotor.run_angle(speed,rotation_angle,Stop.COAST,True)
     
     def cms2degrees(self, cms):
+        "converts centimeters to rotation degrees of the wheels"
         circumfrence = 2*3.14*self.wheelRadiusCm
         conversion = 360.0/circumfrence
         degrees = cms*conversion
         return degrees
 
     def driveForwardCms(self, speed, cms):
+        "drives foward how many centimeters you tell it"
         #convert cm to degrees
         degrees = self.cms2degrees(cms)
         self.driveForward(speed, degrees)
     
     def driveStraightCms(self, speed, cms):
+        "drives straight with gyro sensor, however many cms you tell it"
         degreesTarget = self.cms2degrees(cms)
         intialGyroAngle = self.gyro.angle()
         rotation_angle = self.rightMotor.angle()
+        # TBD: get the robot to slow down as it
+        # gets close to it's destination
         while rotation_angle < degreesTarget:
             gyroAngle = self.gyro.angle()
             error = gyroAngle - intialGyroAngle
@@ -60,10 +71,13 @@ class Robot:
         self.leftMotor.stop()
 
     def spinRightToAngle(self, speed, targetAngle):
+        "spins right until gyro reads the angle that you tell it"
         gyroAngle = self.gyro.angle()
         startingAngle = gyroAngle
         while gyroAngle < targetAngle:
             gyroAngle = self.gyro.angle()
+            # this makes it so that you start going fast in your turn
+            # but then as you get closer to your angle, you slow down
             scale = (gyroAngle - startingAngle) / (targetAngle - startingAngle)
             rampSpeed = speed * (1 - scale)
             rampSpeed = max(rampSpeed, self.minSpeed)
@@ -75,6 +89,7 @@ class Robot:
         self.leftMotor.stop(Stop.BRAKE)
 
     def spinLeftToAngle(self, speed, targetAngle):
+        "spins left until gyro reads the angle that you tell it"
         gyroAngle = self.gyro.angle()
         startingAngle = gyroAngle
         while gyroAngle > targetAngle:

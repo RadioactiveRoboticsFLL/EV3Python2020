008381f3f714ec91b4f219d16594cc2c5d60b821
RadioactiveRoboticsFLL <RadioactiveRoboticsFLL@gmail.com>
2020-09-04 15:12:31 -0400

    added bocciBench launch

--- launches.py
+++ launches.py
@@ -9,4 +9,13 @@ def pullUpBar():
     # r.spinRight(250, 120)
     wait(1000)
     r.driveForwardCms(-500, 29)
-    r.runTopMotors(-500, 7*360)
+    r.runTopMotors(-500, 7.5*360)
+
+def bocciBench():
+    r = Robot()
+    r.driveStraightCms(500, 50-5)
+    r.spinLeftToAngle(150, -90)
+    r.driveStraightCms(500, 94-16)
+    # we have arrived at the bocci ball
+    r.rightTopMotor.run_time(500, 1000)
+    r.rightTopMotor.run_time(-500, 1000)

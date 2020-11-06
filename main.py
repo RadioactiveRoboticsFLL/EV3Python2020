04e28c3eff7fcba849a1cff6ad09a9f217228289
= <=>
2020-11-06 17:41:47 -0500

    all launches in menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+# :-)
 
 # pybrick imports
 from pybricks import ev3brick as brick
@@ -13,7 +14,7 @@ from motion import driveForward,spinRight,spinLeft
 from Robot import Robot
 from launches import pullUpBar, bocciBench
 from motorControl import motorControl
-from launches import treadmill
+from launches import treadmill, stepCOUNTER, Bocci, bocciPlainBocci, innovationbench
 
 r = Robot()
 
@@ -22,23 +23,32 @@ r.gyroDriftCheck()
 btns = brick.buttons()
 
 brick.display.text("")
-brick.display.text("LEFT == motorControl")
-brick.display.text("RIGHT == pullUpBars")
-brick.display.text("CENTER == bocciBench")
+brick.display.text("LEFT == innavationBench")
+brick.display.text("RIGHT == pullUpBar")
+brick.display.text("CENTER == stepCOUNTER")
+brick.display.text("TOP == bocciPlainBocci")
+brick.display.text("BOTTOM == treadmill")
+
 while True:
     if len(btns) == 1:
         btn = btns[0]
         if btn == Button.LEFT:
-            motorControl()
+            # motorControl()
+            # stepCOUNTER()
+            # Bocci()
+            innovationbench()
         elif btn == Button.RIGHT:
             pullUpBar()
             # r.driveStraightCms(500, 100)
         elif btn == Button.UP:    
-            r.runTopMotors(500, 6*360)
+            # r.runTopMotors(500, 6*360)
+            bocciPlainBocci()
         elif btn == Button.DOWN:
-            r.runTopMotors(-500, 6*360)
+            # r.runTopMotors(-500, 6*360)
+            treadmill()
         elif btn == Button.CENTER:
-            bocciBench()
+            # bocciBench()
+            stepCOUNTER()
 
     btns = brick.buttons()
 

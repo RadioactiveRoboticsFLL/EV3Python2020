8cf16ab5d9bd40d873ca547c223f40f13c4c6c27
RadioactiveRoboticsFLL <RadioactiveRoboticsFLL@gmail.com>
2021-04-12 18:43:30 -0400

    menu now does bocciOtherTable and benchAll

--- menu.py
+++ menu.py
@@ -1,19 +1,19 @@
 from pybricks import ev3brick as brick
 from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
-# :-)   
-from launches import pullUpBar, bocciBench, stepCOUNTER, basketLift
-from launches import treadmill, blueTriangle, Bocci, bocciPlainBocci, innovationbench, bocciOtherTable
+  
+from launches import pullUpBar, bocciBench, stepCOUNTER, basketLift, benchAll
+from launches import treadmill, blueTriangle, Bocci, bocciPlainBocci, innovationbench, bocciOtherTable, BASKETbocci
 
 # displays which button runs each launch
 def displayMENU():
     # top left center bottom right
     brick.display.text("")
     brick.display.text("TOP == bocciPlainBocci")
-    brick.display.text("LEFT == innavationBench")
+    brick.display.text("LEFT == benchAll")
     brick.display.text("CENTER == blueTriangle")
-    brick.display.text("BOTTOM == bocciOtherTable")
     brick.display.text("RIGHT == pullUpBar")
+    brick.display.text("BOTTOM == BocciOtherTable")
 
 # the menu fuction allows you to choose what
 # launch to do
@@ -27,13 +27,14 @@ def menu():
         if len(btns) == 1:
             btn = btns[0]
             if btn == Button.LEFT:
-                innovationbench()
+                benchAll()
             elif btn == Button.RIGHT:
                 pullUpBar()
             elif btn == Button.UP:    
                 bocciPlainBocci()
             elif btn == Button.DOWN:
                 bocciOtherTable()
+                # BASKETbocci()
                 # basketLift()
             elif btn == Button.CENTER:
                 blueTriangle()

23d782ae75abf071863141436ccde2e063041e2e
radioactiveroboticsfll <radioactiveroboticsfll@gmail.com>
2021-06-08 18:28:19 -0400

    started animation of robot

--- pyGame.py
+++ pyGame.py
@@ -1,6 +1,7 @@
 # this draws the mat and line of the were the robot went
 
 import pygame
+import math
 
 
 def runGame(positions):
@@ -60,7 +61,20 @@ def runGame(positions):
     matImg = pygame.image.load(fn)
     matImg = pygame.transform.scale(matImg, (matDisplayWidth, matDisplayHeight))
 
+    # Grab img for animation 
+    fn = "data/robotPic1.jpg"
+    botImg = pygame.image.load(fn)
+    botImg = pygame.transform.scale(botImg, (100, 100))
 
+    # set it up to move on it's first line
+    deg2rad = (math.pi/180.0)
+    botX = displayPositions[0][0]
+    botY = displayPositions[0][1]
+    botX2 = displayPositions[1][0]
+    botY2 = displayPositions[1][1]
+    botAngle = math.tanh((botY2 - botY)/(botX2 - botX)) / deg2rad
+    botSpeed = 0.5
+    
     running = True
     while running:
 
@@ -73,6 +87,20 @@ def runGame(positions):
         # this is were we make the background a color and draws a Picture 
         gameDisplay.fill(white)
         gameDisplay.blit(matImg, (matStartPixels, 0))
+
+        # display bot
+        # gameDisplay.blit(botImg, (int(botX), int(botY)))
+
+        # update bot position
+        speedX = botSpeed * math.cos(deg2rad*botAngle)
+        speedY = botSpeed * math.sin(deg2rad*botAngle)
+        # if int(botX) == int(botX2) and int(botY) == int(botY2):
+        if abs(botX - botX2) < 3 and abs(botY - botY2) < 3:
+            # we got to the end of the line!
+            speedX = 0
+            speedY = 0
+        botX = botX + (speedX * 1)
+        botY = botY + (speedY * 1)
 
         # display positions:
         for i in range(len(displayPositions)-1):
